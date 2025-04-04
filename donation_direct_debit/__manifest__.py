58a89b0b382acf63755a67ef45f4769a7ea463c0
Moses Mwaba <mosesm.zaso@gmail.com>
2025-04-04 10:35:16 +0200

    fixed manifest in donation_direct_debit

--- donation_direct_debit/__manifest__.py
+++ donation_direct_debit/__manifest__.py
@@ -5,7 +5,7 @@
 
 {
     "name": "Donation Direct Debit",
-    "version": "18.0.1.0.0",
+    "version": "18.0",
     "category": "Accounting",
     "license": "AGPL-3",
     "summary": "Auto-generate direct debit order on donation validation",
